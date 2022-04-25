97032aaae9bdc34d67f6a5d86fdd951b7352ec3d
zwcwl <zc15722@163.com>
2022-04-25 17:58:31 +0800

    2022-4-2

--- src/components/home-view/HomeHero.vue
+++ src/components/home-view/HomeHero.vue
@@ -2,7 +2,7 @@
     <div class="home-hero">
         <div class="home-swiper">
             <ul>
-                <li><a href=""><img src="" alt=""></a></li>
+                <li><a href=""><img src="@/assets/images/002.webp" alt=""></a></li>
                 <li><a href=""><img src="" alt=""></a></li>
                 <li><a href=""><img src="" alt=""></a></li>
                 <li><a href=""><img src="" alt=""></a></li>
@@ -20,20 +20,7 @@
             </div>
         </div>
 
-        <div class="home-list">
-            <ul>
-                <li><a href=""><span>Xiaomi手机</span></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-            </ul>
-        </div>
+        
 
         <div class="home-sub clearfix">
             <div class="home-channel-list">
@@ -59,142 +46,109 @@
 
 <script>
 export default {
-    name:"home-hero"
-}
+  name: "home-hero",
+};
 </script>
 
 <style lang="scss">
-.home-hero{
-    width: 1226px;
-    margin: 0 auto;
+.home-hero {
+  width: 1226px;
+  margin: 0 auto;
 }
 
-.home-swiper{
-    position: relative;
-    ul li{
-        position: absolute;
-        height: 460px;
-        width: 1226px;
-        top: 0;
-        left: 0;
-    }
-    .btn-rl{
-        position: absolute;
-        button{
-            width: 40px;
-            height: 70px;
-            border-radius: 6px;
-            background-color: rgb(255, 75, 105);
-            position: absolute;
-            top: calc(460px / 2 - 35px);
-            i{
-                font-size: 24px;
-            }
-        }
-        .btn-left{
-            left: 234px;
-        }
-        .btn-right{
-            right: -1226px;
-            transform: rotateZ(180deg);
-        }
-    }
-    .btn-pagination{
-        position: absolute;
-        right: 26px;
-        bottom: -436px;
-        a{
-            width: 6px;
-            height: 6px;
-            border-radius: 50%;
-            border: 2px solid #999999;
-            float: left;
-            margin: 4px;
-            background-color: #fff;
-        }
-    }
-}
-
-.home-list{
-    width: 234px;
+.home-swiper {
+  position: relative;
+  height: 460px;
+  ul li {
+    position: absolute;
     height: 460px;
-    background-color: rgba($color: #919191, $alpha: .3);
-    position: relative;
-    z-index: 10;
-
-    ul{
-        padding: 20px 0;
-
-        li{
-            height: 42px;
-            a{
-                display: block;
-                height: 100%;
-                width: 100%;
-                line-height: 42px;
-                position: relative;
-                text-indent: 16px;
-                color: white;
-
-                &::after{
-                    content: ">";
-                    position: absolute;
-                    right: 16px;
-                    top: 0;
-                    display: block;
-                    height: 42px;
-                    
-                }
-            }
-        }
+    width: 1226px;
+    top: 0;
+    left: 0;
+  }
+  .btn-rl {
+    position: absolute;
+    button {
+      width: 40px;
+      height: 70px;
+      border-radius: 6px;
+      background-color: rgb(255, 75, 105);
+      position: absolute;
+      top: calc(460px / 2 - 35px);
+      i {
+        font-size: 24px;
+      }
     }
+    .btn-left {
+      left: 234px;
+    }
+    .btn-right {
+      right: -1226px;
+      transform: rotateZ(180deg);
+    }
+  }
+  .btn-pagination {
+    position: absolute;
+    right: 26px;
+    top: 436px;
+    a {
+      width: 6px;
+      height: 6px;
+      border-radius: 50%;
+      border: 2px solid hsla(0,0%,100%,.3);
+      float: left;
+      margin: 4px;
+      background: rgba(0,0,0,.4);
+    }
+  }
 }
 
-.home-sub{
-    margin: 14px 0 26px;
-    height: 170px;
-    .home-channel-list{
-        float: left;
-        height: 100%;
-        width: 234px;
-        margin-right: 14px;
-        ul{
-            display: flex;
-            padding: 3px;
-            align-content: space-between;
-            justify-content: space-between;
-            flex-wrap: wrap;
-            li{
-                width: 70px;
-                height: 82px;
-                padding: 0 3px;
-                background-color: rgb(145, 95, 95);
-                text-align: center;
-                i{
-                    font-size: 32px;
-                    margin-top: 6px;
-                    display: block;
-                }
-                p{
-                    margin-top: -6px;
-                }
-            }
+.home-sub {
+  margin: 14px 0 26px;
+  height: 170px;
+
+  .home-channel-list {
+    float: left;
+    height: 100%;
+    width: 234px;
+    margin-right: 14px;
+    background-color: rgb(145, 95, 95);
+    ul {
+      display: flex;
+      padding: 3px;
+      align-content: space-between;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      li {
+        width: 70px;
+        height: 82px;
+        padding: 0 3px;
+        text-align: center;
+        i {
+          font-size: 32px;
+          margin-top: 6px;
+          display: block;
         }
-    }
-    .home-promo-list{
-        float: left;
-        height: 100%;
-        width: calc(1226px - 248px);
-        ul{
-            display: flex;
-            align-items: stretch;
-            justify-content: space-between;
-            li{
-                width: 316px;
-                height: 170px;
-                background-color: rgb(216, 151, 151);
-            }
+        p {
+          margin-top: -6px;
         }
+      }
     }
+  }
+  .home-promo-list {
+    float: left;
+    height: 100%;
+    width: calc(1226px - 248px);
+    ul {
+      display: flex;
+      align-items: stretch;
+      justify-content: space-between;
+      li {
+        width: 316px;
+        height: 170px;
+        background-color: rgb(216, 151, 151);
+      }
+    }
+  }
 }
 </style>
--- src/components/public-view/SiteHeader.vue
+++ src/components/public-view/SiteHeader.vue
@@ -8,6 +8,25 @@
       </div>
       <div class="header-nav">
         <ul class="nav-list clearfix">
+          <li class="nav-category">
+            <a href="">全部商品分类</a>
+            <div class="site-category">
+            <ul class="site-category-list">
+                <li class="category-item" @mouseenter="showChildren(true)" @mouseleave="showChildren(false)" v-for="(item) of siteCategory" :key="item.title"><a class="show-active" href=""><span>{{item.title}}</span></a>
+                  <div class="children">
+                    <ul class="children-list" :style="{width:248*item.num+'px'}">
+                      <li v-for="obj of item.itemList" :key="obj.describe">
+                        <a href="">
+                          <img src="@/assets/images/04.webp" alt="">
+                          <span>{{obj.describe}}</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+            </ul>
+        </div>
+          </li>
           <li v-for="(item,index) of commodity" class="nav-item" :key="item.description" @mouseenter="navListShow(index)" @mouseleave="navListConceal">
             <a href="">{{ item.description }}</a>
           </li>
@@ -16,21 +35,14 @@
       <div class="header-search">
         <form class="search-form clearfix" @mouseenter="formWrie(true)" @mouseleave="formWrie(false)">
           <a href="javascript:void(0);" class="search-text">
-            <input type="text" @focus="formListIsShow(true)" @blur="formListIsShow(false)">
+            <input type="text" :placeholder="placeholder" @focus="formListIsShow(true)" @blur="formListIsShow(false)">
           </a>
           <a href="javascript:void(0);" class="search-btn" @mouseenter="btnAlter(true)" @mouseleave="btnAlter(false)">
             <i class="iconfont icon-sousuo"></i>
           </a>
           <div class="keyword-list">
             <ul class="result-list">
-              <li><a href="">hello</a></li>
-              <li><a href="">world</a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
+              <li v-for="item of keyWords" :key="item" @mouseenter="bgcActive(true)" @mouseleave="bgcActive(false)"><a href="javascript:;">{{item}}</a></li>
             </ul>
           </div>
         </form>
@@ -146,7 +158,140 @@ export default {
       ],
       arr: [],
       toogle: false,
+      keyWords: [
+        "全部商品",
+        "小米手机",
+        "红米手机",
+        "小米平板 5 Pro",
+        "洗衣机",
+        "冰箱",
+        "耳机",
+        "显示器",
+      ],
+      placeholder: "",
+      siteCategory: [
+        {
+          title: "手机",
+          num:2,
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "2 Pro",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "0",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "3 Civi 1S",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "5 Pro",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "36",
+            },
+            {
+              image: "@/assets/images/04.webp",
+              describe: "5554",
+            },
+          ],
+        },
+        /*{
+          title: "电视",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "笔记本 平板",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "家电",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "出行 穿戴",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "智能 路由器",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "电源 配件",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "健康 儿童",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "耳机 音箱",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },
+        {
+          title: "生活 箱包",
+          itemList: [
+            {
+              image: "@/assets/images/04.webp",
+              describe: "Xiaomi Civi 1S",
+            },
+          ],
+        },*/
+      ]
     };
+  },
+  computed:{
+    pxSize(){
+      return this.siteCategory
+    }
   },
   methods: {
     navListShow(num) {
@@ -173,26 +318,57 @@ export default {
         event.target.children[1].classList.remove("wrie-active");
       }
     },
-    formListIsShow(toogle){
+    formListIsShow(toogle) {
       if (toogle) {
+        console.log(event.target.parentNode.parentNode.children[0]);
         event.target.parentNode.parentNode.children[2].style.display = "block";
-        event.target.parentNode.parentNode.children[0].style.borderColor = "#ff6a00";
-        event.target.parentNode.parentNode.children[1].style.borderColor = "#ff6a00";
+        event.target.parentNode.parentNode.children[0].classList.add(
+          "click-active"
+        );
+        event.target.parentNode.parentNode.children[1].classList.add(
+          "click-active"
+        );
       } else {
         event.target.parentNode.parentNode.children[2].style.display = "none";
-        event.target.parentNode.parentNode.children[0].style.borderColor = "#e0e0e0";
-        event.target.parentNode.parentNode.children[1].style.borderColor = "#e0e0e0";
+        event.target.parentNode.parentNode.children[0].classList.remove(
+          "click-active"
+        );
+        event.target.parentNode.parentNode.children[1].classList.remove(
+          "click-active"
+        );
       }
     },
-    btnAlter(toogle){
-      if(toogle){
+    btnAlter(toogle) {
+      if (toogle) {
         event.target.classList.add("btn-active");
-        event.target.style.borderColor = "#ff6a00";
-      }else{
+      } else {
         event.target.classList.remove("btn-active");
-        event.target.style.borderColor = "#e0e0e0";
+      }
+    },
+    bgcActive(toogle) {
+      if (toogle) {
+        event.target.style.backgroundColor = "#fafafa";
+      } else {
+        event.target.style.backgroundColor = "#fff";
+      }
+    },
+    showChildren(toogle){
+      if(toogle){
+        console.log(event.target.children[1]);
+        event.target.children[1].style.display="block"
+        event.target.classList.add("bgc-active");
+      }else{
+        event.target.children[1].style.display="none"
+        event.target.classList.remove("bgc-active");
       }
     }
+  },
+  mounted() {
+    let num = this.keyWords.length;
+    setInterval(() => {
+      this.placeholder = this.keyWords[num % this.keyWords.length];
+      num++;
+    }, 5000);
   },
 };
 </script>
@@ -204,6 +380,7 @@ export default {
   .container {
     width: 1226px;
     margin: 0 auto;
+    position: relative;
   }
 }
 .header-logo {
@@ -222,10 +399,10 @@ export default {
 }
 
 .header-nav {
-  padding: 12px 0 0 30px;
+  padding: 12px 0 0 0;
   float: left;
   height: 88px;
-  margin-left: 142px;
+  margin-left: 30px;
 
   .nav-list .nav-item {
     float: left;
@@ -240,6 +417,92 @@ export default {
       color: #ff6a00;
     }
   }
+
+  .nav-list .nav-category {
+    float: left;
+    width: 127px;
+    margin-right: 15px;
+    & > a {
+      display: block;
+      padding: 26px 0 38px;
+      color: #333333;
+      font-size: 16px;
+      text-align: right;
+      transition: color 0.3s;
+    }
+    .site-category {
+      width: 234px;
+      height: 460px;
+      background: rgba(105,101,101,.6);
+      position: absolute;
+      z-index: 10;
+      left: 0;
+      .site-category-list {
+        padding: 20px 0;
+
+        .category-item {
+          height: 42px;
+          position: relative;
+          .show-active {
+            display: block;
+            height: 100%;
+            width: 100%;
+            line-height: 42px;
+            position: relative;
+            text-indent: 16px;
+            color: white;
+
+            &::after {
+              content: ">";
+              position: absolute;
+              right: 16px;
+              top: 0;
+              display: block;
+              height: 42px;
+            }
+          }
+          &.bgc-active{
+            background-color: #ff6a00;
+          }
+          .children{
+            position: absolute;
+            top: -20px;
+            left: 234px;
+            height: 460px;
+            display: none;
+            box-shadow: 0 8px 16px rgba(0,0,0,.18);
+            background-color: #fff;
+            .children-list{
+              height: 456px;
+              width: calc(248px * 2);
+              display: flex;
+              padding: 2px 0;
+              flex-direction: column;
+              flex-wrap: wrap;
+              li{
+                width: 248px;
+                height: 76px;
+                a{
+                  display: block;
+                  padding: 18px 20px;
+                  height: 40px;
+                  img{
+                    float: left;
+                  }
+                  span{
+                    float: left;
+                    height: 40px;
+                    line-height: 40px;
+                    color: #000;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 .header-search {
@@ -251,6 +514,7 @@ export default {
     .search-text {
       float: left;
       border: 1px solid #e0e0e0;
+      border-right-style: none;
       width: 223px;
       padding: 0 10px;
       input {
@@ -260,7 +524,6 @@ export default {
     .search-btn {
       float: left;
       border: 1px solid #e0e0e0;
-      border-left-style: none;
       width: 49px;
       height: 48px;
       i {
@@ -270,13 +533,23 @@ export default {
         text-align: center;
       }
     }
-    .btn-active{
-      background-color: #ff6a00;
-      color: #616161;
-    }
-    .wrie-active{
+
+    .wrie-active {
       border-color: #b0b0b0;
     }
+
+    .btn-active {
+      background-color: #ff6a00;
+      border-color: #ff6a00;
+      i {
+        color: white;
+      }
+    }
+
+    .click-active {
+      border-color: #ff6a00;
+    }
+
     .keyword-list {
       position: absolute;
       top: 50px;
@@ -315,7 +588,6 @@ export default {
   height: 0;
   overflow: hidden;
   background-color: #fff;
-  border-top: 1px solid #ccc;
   opacity: 0;
   transition: height 0.3s;
 
@@ -371,13 +643,27 @@ export default {
 
   &.slide-down {
     opacity: 1;
-    height: 231px;
+    height: 230px;
   }
 
   &.slide-up {
     transition: height 0.3s, opacity 0.8s;
     opacity: 0;
     height: 0;
+    &::after {
+      border-color: transparent;
+    }
+  }
+
+  &::after {
+    content: "";
+    display: block;
+    width: 100%;
+    border-top: 1px solid #ccc;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: border-color 0.5s;
   }
 }
 </style>
