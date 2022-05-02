49ff00159296a904119a6be8e1a75a3bf9ebb912
zwcwl <zc15722@163.com>
2022-05-02 18:08:18 +0800

    2022-5-2

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -30,24 +30,70 @@
       <div class="layout-container">
         <div class="qr-switch-wrap"></div>
         <div class="ant-tabs">
-          <div class="tablist" clearfix>
-            <div @click="toogle=true" :class="{'col-active':toogle}" class="dl"><a href="javascript:;">登入</a></div>
-            <div @click="toogle=false" :class="{'col-active':!toogle}" class="zc"><a href="javascript:;">注册</a></div>
-            <div :class="{'bor-active':!toogle}" class="bor"></div>
+          <div class="tablist"
+               clearfix>
+            <div @click="toogle=true"
+                 :class="{'col-active':toogle}"
+                 class="dl"><a href="javascript:;">登入</a></div>
+            <div @click="toogle=false"
+                 :class="{'col-active':!toogle}"
+                 class="zc"><a href="javascript:;">注册</a></div>
+            <div :class="{'bor-active':!toogle}"
+                 class="bor"></div>
           </div>
           <div class="content-holder clearfix">
             <div class="ant-tabs-tabpane">
-              <form action="" class="mi-form">
+              <form action=""
+                    class="mi-form">
                 <div class="mi-form-content">
-                  <div class="user-name"></div>
-                  <div class="user-pswd"></div>
-                  <div class="accept-terms"></div>
-                  <div class="btn"></div>
-                  <div class="mode"></div>
+                  <div class="user-name">
+                    <div class="mi-input">
+                      <table>邮箱/手机号码/小米ID</table>
+                      <input type="text">
+                    </div>
+                    <div class="text-error">
+                      <span>请输入账号</span>
+                    </div>
+                  </div>
+
+                  <div class="user-pswd">
+                    <div class="mi-input">
+                      <table>密码</table>
+                      <input type="password">
+                    </div>
+                    <div class="text-error">
+                      <span>请输入账号</span>
+                    </div>
+                  </div>
+                  <div class="accept-terms clearfix">
+                    <div class="ant-checkbox">
+                      <input type="checkbox">
+                      <span></span>
+                    </div>
+                    <div>
+                      已阅读并同意小米账号
+                      <a href="">用户协议</a>
+                      <a href="">隐私政策</a>
+                    </div>
+                  </div>
+                  <div class="btn">
+                    <button>登入</button>
+                  </div>
+                  <div class="mode">
+                    <a href="">忘记密码</a>
+                    <a href="">手机号登入</a>
+                  </div>
                 </div>
                 <div class="mi-sns-login">
-                  <div class="sns-login-title"></div>
-                  <div class="sns-login-content"></div>
+                  <div class="sns-login-title">其他登入方式</div>
+                  <div class="sns-login-content">
+                    <ul>
+                      <li><a href=""></a></li>
+                      <li><a href=""></a></li>
+                      <li><a href=""></a></li>
+                      <li><a href=""></a></li>
+                    </ul>
+                  </div>
                 </div>
               </form>
             </div>
@@ -64,14 +110,14 @@
 <script>
 export default {
   name: "LoginView",
-  data() {
+  data () {
     return {
       toogle: true
     }
   },
-  methods:{
-    cut(){
-      
+  methods: {
+    cut () {
+
     }
   }
 }
@@ -85,7 +131,6 @@ export default {
     bottom: 0;
     left: 0;
     width: 375px;
-    background-color: pink;
     background-image: url('@/assets/images/login-01.jpg');
     background-size: cover;
     background-repeat: no-repeat;
@@ -164,23 +209,23 @@ export default {
     }
     .layout-container {
       padding: 40px 45px;
-      border: 1px solid pink;
-      .qr-switch-wrap{
+      background-color: pink;
+      .qr-switch-wrap {
         position: absolute;
       }
-      .ant-tabs{
-        .tablist{
+      .ant-tabs {
+        .tablist {
           position: relative;
           height: 40px;
           margin-bottom: 16px;
-          .dl{
+          .dl {
             float: left;
             margin-right: 20px;
           }
-          .zc{
+          .zc {
             float: left;
           }
-          .bor{
+          .bor {
             position: absolute;
             width: 44px;
             height: 4px;
@@ -189,34 +234,123 @@ export default {
             left: 0;
             bottom: 0;
           }
-          a{
+          a {
             font-size: 22px;
           }
-          .col-active a{
+          .col-active a {
             color: #000;
           }
-          .bor-active{
+          .bor-active {
             left: 64px;
           }
         }
-        .content-holder{
+        .content-holder {
           padding: 20px 2px 0;
-          .ant-tabs-tabpane{
-            .mi-form{
-              .mi-form-content{
-                .user-name{
-                  
+          .ant-tabs-tabpane {
+            .mi-form {
+              .mi-form-content {
+                .user-name {
+                  position: relative;
+                  .mi-input {
+                    width: 354px;
+                    height: 60px;
+                    background-color: pink;
+                    table {
+                      position: absolute;
+                      z-index: 10;
+                      left: 20px;
+                      font-size: 17px;
+                      height: 20px;
+                      top: 20px;
+                      line-height: 20px;
+                    }
+                    input {
+                      position: absolute;
+                      width: 100%;
+                      padding: 30px 20px 10px;
+                      height: 20px;
+                      border-radius: 6px;
+                    }
+                  }
+                  .text-error {
+                    margin-top: 10px;
+                    height: 20px;
+                    line-height: 20px;
+                    font-size: 12px;
+                  }
                 }
-                .user-pswd{
-
+                .user-pswd {
+                  margin-top: 20px;
+                  position: relative;
+                  border-radius: 3px;
+                  .mi-input {
+                    width: 354px;
+                    height: 60px;
+                    background-color: pink;
+                    table {
+                      position: absolute;
+                      z-index: 10;
+                      left: 20px;
+                      font-size: 17px;
+                      height: 20px;
+                      top: 20px;
+                      line-height: 20px;
+                    }
+                    input {
+                      position: absolute;
+                      width: 100%;
+                      padding: 30px 20px 10px;
+                      height: 20px;
+                      border-radius: 6px;
+                    }
+                  }
+                  .text-error {
+                    margin-top: 10px;
+                    height: 20px;
+                    line-height: 20px;
+                    font-size: 12px;
+                  }
                 }
-                .accept-terms{
-
+                .accept-terms {
+                  margin-top: 20px;
+                  height: 20px;
+                  .ant-checkbox {
+                    float: left;
+                    position: relative;
+                    width: 20px;
+                    height: 20px;
+                    margin-right: 3px;
+                    input {
+                      height: 16px;
+                      width: 16px;
+                      border-radius: 3px;
+                      border: 2px solid pink;
+                      opacity: 0;
+                    }
+                    span {
+                      position: absolute;
+                      top: 0;
+                      left: 0;
+                      display: block;
+                      width: 18px;
+                      height: 18px;
+                      border-radius: 3px;
+                      border: 1px solid #b0b0b0;
+                    }
+                  }
                 }
-                .btn{
-
+                .btn {
+                  margin: 20px 0 10px;
+                  button {
+                    height: 60px;
+                    border-radius: 3px;
+                    font-size: 18px;
+                    line-height: 60px;
+                    background-color: #ffbe99;
+                    width: 100%;
+                  }
                 }
-                .mode{
+                .mode {
 
                 }
               }
